9e7b37097dc680a0882941062c8d2f52340c9308
Himani Bhutra <himanibhutra2003@gmail.com>
2025-07-08 18:59:53 +0530

    Updated the review section and updated the style for the review box

--- content.js
+++ content.js
@@ -9,7 +9,7 @@ window.addEventListener("load", function () {
     box.style.display = "none"; 
   }, 3000);
 
-  /*setTimeout(() => {
+  setTimeout(() => {
     const review = extract_reviews();
 
     summarizeReviews(review, function(err, summary){
@@ -17,17 +17,9 @@ window.addEventListener("load", function () {
         console.error("Failed to summarize!",err);
         return;
       }
-       const summaryBox = document.createElement("div");
-      summaryBox.style.background = "#fff8dc";
-      summaryBox.style.padding = "12px";
-      summaryBox.style.marginTop = "20px";
-      summaryBox.style.border = "1px solid #ccc";
-      summaryBox.style.borderRadius = "8px";
-      summaryBox.style.maxWidth = "600px";
-      summaryBox.style.boxShadow = "0 0 8px rgba(0,0,0,0.1)";
+      const summaryBox = document.createElement("div");
+      summaryBox.id = "summary_box"
       summaryBox.innerHTML = `<b>📝 Review Summary:</b><br>${summary}`;
-
-    
       const reviewSection = document.querySelector("#item-review-section");
       if (reviewSection) {
         reviewSection.prepend(summaryBox);
@@ -36,5 +28,4 @@ window.addEventListener("load", function () {
       }
     });
   }, 10000);
-*/
 });
